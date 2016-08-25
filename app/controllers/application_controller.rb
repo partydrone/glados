c715ed24f04f1fa8b58a25c72bc81ba75d602857
Andrew Porter <andrew.porter@wavetronix.com>
2016-08-25 10:57:26 -0600

    Make locales part of generated URLs
    Without this bit of code, Rails URL generators like `link_to` won’t
    include the locale as part of the generated URL.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,9 @@ class ApplicationController < ActionController::Base
 
   before_action :set_locale
 
-  # def default_url_options(options = {})
-  #   { locale: I18n.locale }.merge options
-  # end
+  def default_url_options(options = {})
+    { locale: I18n.locale }.merge options
+  end
 
   private
 
