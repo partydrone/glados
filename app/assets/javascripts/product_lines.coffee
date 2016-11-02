59c23bfe7e47fe295a3471b570fb174b45f158fb
Andrew Porter <partydrone@me.com>
2016-11-02 09:55:21 -0600

    Convert CoffeeScript files to Javascript and clean up
    I converted the CoffeeScript files to Javascript to keep things
    consistent, as well as looking forward towards ES6.
    
    I also moved the Owl Carousel script our of the main `application.js`
    file and into its own file to keep things compartmentalized and easy to
    find _and_ to manage.

--- app/assets/javascripts/product_lines.coffee
+++
@@ -1,6 +0,0 @@
-jQuery ->
-  $("#product_types").sortable
-    axis: "y"
-    handle: ".handle"
-    update: ->
-      $.post($(@).data("update-path"), $(@).sortable("serialize"))
---
+++ app/assets/javascripts/product_types.js
@@ -0,0 +1,9 @@
+jQuery(function() {
+  $("#product_types").sortable({
+    axis:   "y",
+    handle: ".handle",
+    update: function() {
+      return $.post($(this).data("update-path"), $(this).sortable("serialize"));
+    }
+  });
+});
