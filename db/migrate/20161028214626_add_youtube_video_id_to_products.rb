d3db5dd2f962b7c4b0d193089ee94db16e6c3ffa
Andrew Porter <partydrone@me.com>
2016-11-07 07:20:32 -0700

    Fix typo in database migration

--- db/migrate/20161028214626_add_youtube_video_id_to_products.rb
+++ db/migrate/20161028214626_add_youtube_video_id_to_products.rb
@@ -1,4 +1,4 @@
-class AddYoutubeVideoIdToProducts < ActiveRecord::Migration[5.0]
+class AddYouTubeVideoIdToProducts < ActiveRecord::Migration[5.0]
   def change
     add_column :products, :youtube_video_id, :string
   end
