11a4e0f6645e7300cd356b360a1c1dbd9244c799
Andrew Porter <partydrone@me.com>
2016-11-18 16:27:02 -0700

    Start working on the Marketing app form

--- app/mailers/site_mailer.rb
+++ app/mailers/site_mailer.rb
@@ -1,8 +1,21 @@
 class SiteMailer < ApplicationMailer
   def demo_request(demo_request)
     @demo_request = demo_request
-    mail to: %("Sales" <partydrone@gmail.com>),
+
+    case @demo_request.country
+    when condition
+
+    end
+
+    mail to:       %("Sales" <sales@wavetronix.com>),
          reply_to: %("#{@demo_request.name}" <#{@demo_request.email}>),
-         subject: 'Demo Request'
+         subject:  'Demo Request'
+  end
+
+  def marketing_app_support_request(marketing_app_support_request)
+    @marketing_app_support_request
+    mail to:       %("Marketing Support" <marketing@wavetronix.com>),
+         reply_to: %("#{@marketing_app_support_request.name}" <#{@marketing_app_support_request.email}>),
+         subject:  "Marketing App Support Request"
   end
 end
