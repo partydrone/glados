b3fd38f2e29c05da8430e944adfa61b4f4ba054c
Andrew Porter <partydrone@me.com>
2016-11-29 13:14:14 -0700

    Update config for separate asset server
    The configuration for a separate asset server got lost—probably my
    fault. I’m putting the configuration back in.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -4,5 +4,9 @@ Rails.application.configure do
   # Overwrite any production settings here, or if you want to start from
   # scratch, then remove line 1.
 
+  # Full error reports are enabled.
   config.consider_all_requests_local = true
+
+  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
+  config.action_controller.asset_host = '//dw8wks5reei42.cloudfront.net'
 end
