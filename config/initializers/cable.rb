cf6bbd8d14076168c0c8972f0793a4fe595e75ea
Andrew Porter <partydrone@me.com>
2016-11-29 12:23:48 -0700

    Remove Redis and Cable servers
    Trying to get the staging environment to run the app. It looks like it
    may be getting hung up on the cable server configuration.

--- config/initializers/cable.rb
+++ config/initializers/cable.rb
@@ -1,9 +1,9 @@
-Rails.application.configure do
-  # Action Cable setting to de-couple it from the main Rails process.
-  config.action_cable.url = ENV['ACTION_CABLE_FRONTEND_URL']
-
-  # Action Cable setting to allow connections from these domains.
-  origins = ENV['ACTION_CABLE_ALLOWED_REQUEST_ORIGINS'].split(',')
-  origins.map! { |url| /#{url}/ }
-  config.action_cable.allowed_request_origins = origins
-end
+# Rails.application.configure do
+#   # Action Cable setting to de-couple it from the main Rails process.
+#   config.action_cable.url = ENV['ACTION_CABLE_FRONTEND_URL']
+#
+#   # Action Cable setting to allow connections from these domains.
+#   origins = ENV['ACTION_CABLE_ALLOWED_REQUEST_ORIGINS'].split(',')
+#   origins.map! { |url| /#{url}/ }
+#   config.action_cable.allowed_request_origins = origins
+# end
