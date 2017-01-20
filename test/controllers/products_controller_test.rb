45e77d64aa9cb2bd3afa9495f2836df5350f90cf
Andrew Porter <partydrone@me.com>
2017-01-20 10:38:12 -0700

    Fix bugs introduced by implementing admin section

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 describe ProductsController, :locale do
+  it "gets index" do
+    get products_path
+    must_respond_with :success
+  end
+
   it "gets show" do
     get product_path(products(:smartsensor_hd))
     must_respond_with :success
