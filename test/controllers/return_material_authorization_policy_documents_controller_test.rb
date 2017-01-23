b231be80bcbe813a16b5b229c6f9849381842869
Andrew Porter <partydrone@me.com>
2017-01-23 15:11:53 -0700

    Clean up display routes and fix routing bugs

--- test/controllers/return_material_authorization_policy_documents_controller_test.rb
+++ test/controllers/return_material_authorization_policy_documents_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 describe ReturnMaterialAuthorizationPolicyDocumentsController, :locale do
+  it "gets index" do
+    get return_material_authorization_policy_documents_path
+    must_respond_with :success
+  end
+
   it "gets show" do
     get return_material_authorization_policy_document_path(return_material_authorization_policy_documents(:current_rma_policy_document))
     must_respond_with :success
--- test/controllers/website_terms_of_use_documents_controller_test.rb
+++ test/controllers/website_terms_of_use_documents_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 describe WebsiteTermsOfUseDocumentsController, :locale do
+  it "gets index" do
+    get website_terms_of_use_documents_path
+    must_respond_with :success
+  end
+
   it "gets show" do
     get website_terms_of_use_document_path(website_terms_of_use_documents(:current_website_terms_of_use_document))
     must_respond_with :success
