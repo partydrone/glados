289aa2b5e910d9686c502a23973c5fa31faf9118
Andrew Porter <partydrone@me.com>
2017-02-01 12:58:58 -0700

    Revert taggable methods

--- app/models/concerns/taggable.rb
+++ app/models/concerns/taggable.rb
@@ -6,13 +6,23 @@ module Taggable
     has_many :tags, through: :taggings
   end
 
-  def tag(name)
-    name.strip!
-    tag = Tag.find_or_create_by(name: name)
-    self.taggings.find_or_create_by(tag_id: tag.id)
+  class_methods do
+    def tagged_with(name)
+      Tag.find_by_name!(name)
+    end
+
+    def tag_counts
+      Tag.select('tags.*, count(taggings.tag_id) as count').joins(:taggings).group('taggings.tag_id')
+    end
   end
 
   def tag_list
-    tags.collect(&:name)
+    tags.map(&:name).join(', ')
+  end
+
+  def tag_list=(names)
+    self.tags = names.split(', ').map do |n|
+      Tag.where(name: n.strip).first_or_create!
+    end
   end
 end
