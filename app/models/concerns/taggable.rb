e8becde65d5a14e70e13437244479ba56e31bc2c
Andrew Porter <partydrone@me.com>
2017-02-07 09:25:30 -0700

    Clean up or refactor taggable methods

--- app/models/concerns/taggable.rb
+++ app/models/concerns/taggable.rb
@@ -6,24 +6,14 @@ module Taggable
     has_many :tags, through: :taggings
   end
 
-  class_methods do
-    def tagged_with(name)
-      Tag.find_by_name!(name)
-    end
-
-    def tag_counts
-      Tag.select('tags.*, count(taggings.tag_id) as count').joins(:taggings).group('taggings.tag_id')
-    end
-  end
-
   def tag_list
     tags.map(&:name).join(', ')
   end
 
   def tag_list=(names)
-    self.tags = names.split(', ').map do |n|
+    self.tags = names.split(',').map do |n|
       n.downcase!
-      Tag.where(name: n.strip).first_or_create!
+      Tag.find_or_create_by!(name: n.strip)
     end
   end
 end
