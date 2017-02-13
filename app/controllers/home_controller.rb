c3a8e8c074d63fe17da85c1cad908e11467b0a87
Andrew Porter <partydrone@me.com>
2017-02-13 14:57:57 -0700

    Add articles to home page
    Pull in tagged content into each section of the home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,10 @@
 class HomeController < ApplicationController
-  def index; end
+  def index
+    @safe_articles           = Tagging.joins(:tag).where(tags: {name: 'safe'}).limit(6)
+    @efficient_articles      = Tagging.joins(:tag).where(tags: {name: 'efficient'}).limit(6)
+    @reliable_articles       = Tagging.joins(:tag).where(tags: {name: 'reliable'}).limit(6)
+    @cost_effective_articles = Tagging.joins(:tag).where(tags: {name: 'cost effective'}).limit(6)
+    @simple_articles         = Tagging.joins(:tag).where(tags: {name: 'simple'}).limit(6)
+    @support_articles        = Tagging.joins(:tag).where(tags: {name: 'support'}).limit(6)
+  end
 end
