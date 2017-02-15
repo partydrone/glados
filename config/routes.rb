f9ee34c2401a699078187b8cfe8f2ba0ecd36fe8
Jeremy Conterio <jeremy.conterio@wavetronix.com>
2017-02-14 17:59:10 -0700

    Merge branch 'master' into training_events
    # Conflicts:
    #	config/routes.rb
    #	db/schema.rb
    #	db/seeds.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 require 'sidekiq/web'
 
-Rails.application.routes.draw do 
-
-  mount Ckeditor::Engine => '/ckeditor'
+Rails.application.routes.draw do   
   mount Sidekiq::Web => '/sidekiq'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/seeds.rb
+++ db/seeds.rb
@@ -66,13 +66,14 @@ Feature.create([
   { title: 'High-definition Radar', description: 'Feature description', product_ids: [products[2].id, products[3].id, products[4].id] }
 ])
 
-<<<<<<< HEAD
+
 training_courses = TrainingCourse.create([
   { title: 'Introduction to Traffic Radar Workshop', duration: 2, description:'In this workshop, you will learn the basics of radar technology and how it is revolutionizing the traffic detection industry. Familiar with different types of traffic radar and which type  you.'},
   { title: 'Introduction to HD SmartSensor Workshop', duration: 3, description:'In this workshop, you will learn the basics of radar technology and how it is revolutionizing the traffic detection industry. Familiar with different types of traffic radar and which type or you.'},
   { title: 'Introduction to JeremyCool Workshop', duration: 4, description:'In this workshop, you will learn the basics of radar technology and how it is revolutionizing the traffic detection industry. Familiar with different types of traffic radar and d work for you.'}
   
-=======
+])
+
 Patent.create([
   { number: 6556916, title: 'System and Method for Identification of Traffic Lane Positions' },
   { number: 6693557, title: 'Vehicular Traffic Sensor' },
@@ -86,5 +87,5 @@ Patent.create([
   { number: 8248272, title: 'Detecting Targets in Roadway Intersections' },
   { number: 8665113, title: 'Detecting Roadway Targets Across Beams Including Filtering Computed Positions' },
   { number: 9240125, title: 'Detecting Roadway Targets Across Beams' }
->>>>>>> master
+
 ])
