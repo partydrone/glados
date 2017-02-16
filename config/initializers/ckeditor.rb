c4b1f28c88fa2a927b56a1fc7966ff78e81fe725
Andrew Porter <partydrone@me.com>
2017-02-16 14:14:46 -0700

    Allow `.jpeg` extension on image uploads for article body
    Fixes #76.

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -9,7 +9,7 @@ Ckeditor.setup do |config|
   # Allowed image file types for upload.
   # Set to nil or [] (empty array) for all file types
   # By default: %w(jpg jpeg png gif tiff)
-  config.image_file_types = %w(jpg png gif svg)
+  config.image_file_types = %w(jpg jpeg png gif svg)
 
   # Allowed flash file types for upload.
   # Set to nil or [] (empty array) for all file types
