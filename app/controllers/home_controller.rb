e24778bb61eb189580bc83f50f45275a0bbc8271
Andrew Porter <partydrone@me.com>
2017-02-24 18:47:13 -0700

    Filter marketing content for home page and news articles
    This commit meets all the functional requirements of issue #58.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,10 @@
 class HomeController < ApplicationController
   def index
-    @safe_articles           = Tagging.joins(:tag).where(tags: {name: 'safe'}).limit(6)
-    @efficient_articles      = Tagging.joins(:tag).where(tags: {name: 'efficient'}).limit(6)
-    @reliable_articles       = Tagging.joins(:tag).where(tags: {name: 'reliable'}).limit(6)
-    @cost_effective_articles = Tagging.joins(:tag).where(tags: {name: 'cost effective'}).limit(6)
-    @simple_articles         = Tagging.joins(:tag).where(tags: {name: 'simple'}).limit(6)
-    @support_articles        = Tagging.joins(:tag).where(tags: {name: 'support'}).limit(6)
+    @safe_articles           = MarketingContentFilter.new(Tagging.joins(:tag).where(tags: {name: ['safe', 'safety']}).includes(:taggable).map(&:taggable)).filter
+    @efficient_articles      = MarketingContentFilter.new(Tagging.joins(:tag).where(tags: {name: ['efficient', 'efficiency']}).includes(:taggable).map(&:taggable)).filter
+    @reliable_articles       = MarketingContentFilter.new(Tagging.joins(:tag).where(tags: {name: ['reliable', 'reliability']}).includes(:taggable).map(&:taggable)).filter
+    @cost_effective_articles = MarketingContentFilter.new(Tagging.joins(:tag).where(tags: {name: ['cost effective']}).includes(:taggable).map(&:taggable)).filter
+    @simple_articles         = MarketingContentFilter.new(Tagging.joins(:tag).where(tags: {name: ['simple']}).includes(:taggable).map(&:taggable)).filter
+    @support_articles        = MarketingContentFilter.new(Tagging.joins(:tag).where(tags: {name: ['support']}).includes(:taggable).map(&:taggable)).filter
   end
 end
