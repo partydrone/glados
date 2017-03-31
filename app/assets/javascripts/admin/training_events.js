edfe85bcfdb1aea21ead6e7a52b466212c67364c
Jeremy Conterio <jeremy.conterio@wavetronix.com>
2017-03-30 19:30:05 -0600

    fixed javascript to show image for checkbox and add border.  Also fixed the way courses were being pushed and popped, when added (checked)

--- app/assets/javascripts/admin/training_events.js
+++ app/assets/javascripts/admin/training_events.js
@@ -9,33 +9,38 @@ var training_events = {
     //1. checks to see if the course has prerequisites
     //2. checks to see if the course waitlisted(not enough seats available)
     //3. push and pop titles in/out of arrays to display to user    
-    courseCheck: function(check_box) {
-        var child = check_box.children('input');
-        var parent = check_box.parent();
-        //add to array if checked
-        if (child[0].checked) {
-            if (parent.find(".prerequisites").length > 0) {
-                //push title into the prerequisite array
-                training_events.prerequisites.push(parent.find(".prerequisites").attr('id'));
-            } else if (parent.find(".wait-listed").length > 0) {
-                //push title into the waitlisted array
-                training_events.waitlisted.push(parent.find(".wait-listed").attr('id'));
-            }
-        }
-        //remove from array if unchecked 
-        else {
-            if (parent.find(".prerequisites").length > 0) {
+    courseCheck: function(label) {
+        var check_box = label.prev();
+        var training_card = label.parent().parent().parent();
+
+        //remove from array
+        if (check_box[0].checked) {
+            if (training_card.find(".prerequisites").length > 0) {
                 //pop title out of prerequisites array
-                training_events.prerequisites.pop(parent.find(".prerequisites").attr('id'));
-            } else if (parent.find(".wait-listed").length > 0) {
-                //pop title out of waitlisted array
-                training_events.waitlisted.pop(parent.find(".wait-listed").attr('id'));
+                training_events.prerequisites.pop(training_card.find(".prerequisites").attr('id'));
             }
+            if (training_card.find(".wait-listed").length > 0) {
+                //pop title out of waitlisted array
+                training_events.waitlisted.pop(training_card.find(".wait-listed").attr('id'));
+            }
+            training_card.css({ "border-color": "#e6e6e6", "border-width": "2px", "border-style": "solid" });
+        }
+        //add to array
+        else {
+            if (training_card.find(".prerequisites").length > 0) {
+                //push title into the prerequisite array
+                training_events.prerequisites.push(training_card.find(".prerequisites").attr('id'));
+            }
+            if (training_card.find(".wait-listed").length > 0) {
+                //push title into the waitlisted array
+                training_events.waitlisted.push(training_card.find(".wait-listed").attr('id'));
+            }
+            training_card.css({ "border-color": "#54a4da", "border-width": "2px", "border-style": "solid" });
         }
 
     }, //end course check
 
-    validateCourses() {
+    validateCourses: function() {
         // display displayAlert if HAS prerequisites or waitlisted courses
         if (training_events.getDisplayAlert()) {
             alert("you have waitlisted or prerequisites!"); //here is where we will unhide alert div
@@ -45,7 +50,7 @@ var training_events = {
     },
 
     //gets the status of displayAlert
-    getDisplayAlert() {
+    getDisplayAlert: function() {
         //check arrays to see if we have any prerequisites or waitlisted courses
         if (training_events.waitlisted.length > 0 || training_events.prerequisites.length > 0) {
             return true;
@@ -67,4 +72,16 @@ $('.course-check').on('click', function(event) {
 //when user clicks on the submit button validate courses
 $('#submit-btn').on('click', function() {
     return training_events.validateCourses();
+});
+
+
+
+//checkbox event
+$('.input_class_checkbox').each(function() {
+    $(this).hide().after('<div class="class_checkbox" />');
+
+});
+
+$('.class_checkbox').on('click', function() {
+    $(this).toggleClass('checked').prev().prop('checked', $(this).is('.checked'))
 });
