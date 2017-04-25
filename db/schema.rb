c2001e3e6f4d99cdd67a3e140a495299534a7eb2
Andrew Porter <partydrone@me.com>
2017-04-25 11:42:54 -0600

    Migrate the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170413205422) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -285,7 +283,6 @@ ActiveRecord::Schema.define(version: 20170413205422) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 
   create_table "training_course_products", force: :cascade do |t|
     t.integer "training_course_id"
