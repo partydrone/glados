381c0c3ca62d245d9873e95c3be63298709778ad
Jeremy Conterio <jeremy.conterio@wavetronix.com>
2017-04-26 19:09:32 -0600

    filter for course types works

--- app/controllers/training_events_controller.rb
+++ app/controllers/training_events_controller.rb
@@ -1,7 +1,7 @@
 class TrainingEventsController < ApplicationController
   def index
     @training_events = TrainingEvent.all
-    @training_courses = TrainingCourse.all 
+    @training_courses = TrainingCourse.all    
   end
 
   def show    
