9352beb023d706ab64ec8a0551efe24184bfce09
Jeremy Conterio <jeremy.conterio@wavetronix.com>
2017-05-04 10:26:37 -0600

    added the sort by to the rest of the class variables in home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,11 @@
 class HomeController < ApplicationController
   def index
     @safe_articles           = ContentFilter.new(Tagging.joins(:tag).where(tags: {name: ['safe', 'safety']}).includes(:taggable).map(&:taggable)).filter(:marketing_content).sort_by{ |obj| obj.updated_at }.reverse
-    @efficient_articles      = ContentFilter.new(Tagging.joins(:tag).where(tags: {name: ['efficient', 'efficiency']}).includes(:taggable).map(&:taggable)).filter(:marketing_content)
-    @reliable_articles       = ContentFilter.new(Tagging.joins(:tag).where(tags: {name: ['reliable', 'reliability']}).includes(:taggable).map(&:taggable)).filter(:marketing_content)
-    @cost_effective_articles = ContentFilter.new(Tagging.joins(:tag).where(tags: {name: ['cost effective']}).includes(:taggable).map(&:taggable)).filter(:marketing_content)
-    @simple_articles         = ContentFilter.new(Tagging.joins(:tag).where(tags: {name: ['simple']}).includes(:taggable).map(&:taggable)).filter(:marketing_content)
-    @support_articles        = ContentFilter.new(Tagging.joins(:tag).where(tags: {name: ['support']}).includes(:taggable).map(&:taggable)).filter(:marketing_content)
+    @efficient_articles      = ContentFilter.new(Tagging.joins(:tag).where(tags: {name: ['efficient', 'efficiency']}).includes(:taggable).map(&:taggable)).filter(:marketing_content).sort_by{ |obj| obj.updated_at }.reverse
+    @reliable_articles       = ContentFilter.new(Tagging.joins(:tag).where(tags: {name: ['reliable', 'reliability']}).includes(:taggable).map(&:taggable)).filter(:marketing_content).sort_by{ |obj| obj.updated_at }.reverse
+    @cost_effective_articles = ContentFilter.new(Tagging.joins(:tag).where(tags: {name: ['cost effective']}).includes(:taggable).map(&:taggable)).filter(:marketing_content).sort_by{ |obj| obj.updated_at }.reverse
+    @simple_articles         = ContentFilter.new(Tagging.joins(:tag).where(tags: {name: ['simple']}).includes(:taggable).map(&:taggable)).filter(:marketing_content).sort_by{ |obj| obj.updated_at }.reverse
+    @support_articles        = ContentFilter.new(Tagging.joins(:tag).where(tags: {name: ['support']}).includes(:taggable).map(&:taggable)).filter(:marketing_content).sort_by{ |obj| obj.updated_at }.reverse
   end
 
 end
