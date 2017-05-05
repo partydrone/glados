593423af3b0823b15f9dd1b5261f5da8de1c9124
Andrew Porter <partydrone@me.com>
2017-05-05 09:52:26 -0600

    Update content_filter.rb

--- app/services/content_filter.rb
+++ app/services/content_filter.rb
@@ -12,15 +12,12 @@ class ContentFilter
   
   FILTERS = {
     marketing_content: %(
-        ( object.class != BlogPost &&  object.class != CaseStudy && object.class != Feature ) ||
-        ( object.class == BlogPost && !object.published? ) ||
-        ( object.class == CaseStudy && !object.published? ) ||
-        ( object.class == Feature && object.body.blank? )
-     
-      ),
-    support_content:   %(object.class != KnowledgeBaseArticle),
+      ( object.class != BlogPost &&  object.class != CaseStudy && object.class != Feature ) ||
+      ( object.class == BlogPost && !object.published? ) ||
+      ( object.class == CaseStudy && !object.published? ) ||
+      ( object.class == Feature && object.body.blank? )
+    ),
+    support_content: %(object.class != KnowledgeBaseArticle),
     test: %(object.class == String)
   }
 end
-
-
