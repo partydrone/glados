f048cf812487443659c967dfb4a70cdd2f5a5dee
Jeremy Conterio <jeremy.conterio@wavetronix.com>
2017-05-08 11:35:38 -0600

    changed naming for MediaDownloads test

--- test/controllers/media_library_controller_test.rb
+++ test/controllers/media_library_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-describe MediaLibraryController, :locale do
+describe MediaDownloadsController, :locale do
   it "gets index" do
     get media_downloads_path
     must_respond_with :success
