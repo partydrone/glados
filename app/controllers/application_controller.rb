7bd2ccded396cec1755b7b4f1391833e494e818f
Andrew Porter <partydrone@me.com>
2017-05-10 11:53:55 -0600

    Move `protect_from_forgery` to the top of `ApplicationController`
    I’m trying to resolve an `ActionController::InvalidAuthenticityToken`
    issue that popped up in staging, recently.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
-  include Pundit
-
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  include Pundit
 
   before_action :enable_profiler, :set_locale
 
