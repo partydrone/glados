bc5b0da5a47c7e5d00228c1234768be07f97e36a
Elizabeth <ehuntsman@wavetronix.com>
2017-05-12 15:26:55 -0600

    contact

--- app/models/dealer.rb
+++ app/models/dealer.rb
@@ -1,7 +1,12 @@
 class Dealer < ApplicationRecord
 	include PgSearch
-  	multisearchable :against => [:name]
+  	multisearchable against: [:name, :country_id],
+  					using: COUNTRIES
+
+  	def self.search(search)
+
 
 	validates :name, :address, :city, :region, :zip, :phone, :email, :website, presence: true
 
 end
+
