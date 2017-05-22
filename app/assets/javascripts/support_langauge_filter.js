03926a51c2f0c816262d2bd41488cfa4711de2a1
Jeremy Conterio <jeremy.conterio@wavetronix.com>
2017-05-22 17:29:08 -0600

    filtering languages for support download items works

---
+++ app/assets/javascripts/support_langauge_filter.js
@@ -0,0 +1,25 @@
+$(function() {
+
+    $(".languages-types-list").change(function() {
+        var language_type = this.value;
+        //HTML collection       
+        var all_courses = $(".support-download-item");
+
+        //show all if all courses is selected
+        if (language_type == "language-type-all") {
+            all_courses.show();
+            return;
+        }
+        //hide all course cards
+        all_courses.hide();
+
+        //show courses that have the same course-type-id
+        for (var i = 0; i < all_courses.length; ++i) {
+            if (all_courses[i].id == language_type) {
+                all_courses[i].style.display = "block";
+            }
+        }
+
+    });
+
+});
