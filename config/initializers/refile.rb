f6fe0eec6b5106aa54b3da83478aa3deb2955a5d
Andrew Porter <partydrone@me.com>
2017-05-25 21:27:50 -0600

    Update AWS configuration for `refile` uploads to use credentials

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,25 +1,23 @@
-require "refile/s3"
+require 'refile/s3'
 
 case
 when Rails.env.production?
   aws = {
-    access_key_id:     Rails.application.secrets.aws_access_key_id,
-    secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    region:            "us-east-1",
-    bucket:            "com-wavetronix-uploads"
+    credentials: Aws::Credentials.new(Rails.application.secrets.aws_access_key_id, Rails.application.secrets.aws_secret_access_key),
+    region:      'us-east-1',
+    bucket:      'com-wavetronix-uploads'
   }
-  Refile.cdn_host = "//www.wavetronix.com"
+  Refile.cdn_host = '//www.wavetronix.com'
 when Rails.env.staging?
   aws = {
-    access_key_id:     Rails.application.secrets.aws_access_key_id,
-    secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    region:            "us-east-1",
-    bucket:            "com-wavetronix-staging-glados-uploads"
+    credentials: Aws::Credentials.new(Rails.application.secrets.aws_access_key_id, Rails.application.secrets.aws_secret_access_key),
+    region:      'us-east-1',
+    bucket:      'com-wavetronix-staging-glados-uploads'
   }
-  Refile.cdn_host = "//uploads.glados.staging.wavetronix.com"
+  Refile.cdn_host = '//uploads.glados.staging.wavetronix.com'
 end
 
 if Rails.env.production? || Rails.env.staging?
-  Refile.cache = Refile::S3.new(prefix: "cache", **aws)
-  Refile.store = Refile::S3.new(prefix: "store", **aws)
+  Refile.cache = Refile::S3.new(prefix: 'cache', **aws)
+  Refile.store = Refile::S3.new(prefix: 'store', **aws)
 end
