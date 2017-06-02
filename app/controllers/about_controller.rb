c7861cd385d73b3df135b8ce4bfbff9819602551
Andrew Porter <partydrone@me.com>
2017-06-02 16:57:36 -0600

    Update top articles sort criteria

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -3,9 +3,9 @@ class AboutController < ApplicationController
     @recent_articles  = Article.current.news_articles.select(:id, :type, :title, :subtitle, :hero_image_id).order(posted_on: :desc).limit(4).to_a
     @featured_article = @recent_articles.shift
 
-    @top_articles = Article.current.news_articles.select(:id, :type, :title).order(views: :desc).limit(10)
+    @top_articles = Article.current.news_articles.select(:id, :type, :title).order(views: :desc, updated_at: :desc).limit(10)
     @top_tags     = Tag.without_regions.where('taggings_count > 0').reorder(taggings_count: :desc, name: :asc).limit(10)
     @top_regions  = Tag.regions.where('taggings_count > 0').reorder(taggings_count: :desc, name: :asc).limit(10)
-    
+
   end
 end
--- app/models/knowledge_base_article.rb
+++ app/models/knowledge_base_article.rb
@@ -1,8 +1,8 @@
 class KnowledgeBaseArticle < Article
 	include PgSearch
   multisearchable :against => [:title, :body]
-	
-  scope :top, -> { current.select(:id, :type, :title).where('updated_at > ?', 3.months.ago).order(views: :desc) }
+
+  scope :top, -> { current.select(:id, :type, :title).where('updated_at > ?', 3.months.ago).order(views: :desc, updated_at: :desc) }
 
   def digest
     Digest::SHA1.hexdigest title + body
