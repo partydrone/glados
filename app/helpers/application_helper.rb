71914940ed2bccd922863f17752a49a6d39a57cd
Andrew Porter <partydrone@me.com>
2017-06-12 22:39:54 -0600

    Wrap address lines with breaks in them Fixes #247.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,16 +14,16 @@ module ApplicationHelper
 
   def markdown(text)
     options = {
-      filter_html:     true,
-      hard_wrap:       true,
-      link_attributes: { rel: 'nofollow', target: "_blank" },
+      filter_html:         true,
+      hard_wrap:           true,
+      link_attributes:     { rel: 'nofollow', target: "_blank" },
       space_after_headers: true,
-      fenced_code_blocks: true
+      fenced_code_blocks:  true
     }
 
     extensions = {
-      autolink:           true,
-      superscript:        true,
+      autolink:                     true,
+      superscript:                  true,
       disable_indented_code_blocks: true
     }
 
