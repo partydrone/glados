829a20099627bc7758334611ee77188b7a2e8c22
Andrew Porter <partydrone@me.com>
2017-06-13 00:35:36 -0600

    Fix view error when rendering HTML

--- config/initializers/client_side_validations.rb
+++ config/initializers/client_side_validations.rb
@@ -14,8 +14,8 @@
 
 ActionView::Base.field_error_proc = proc do |html_tag, instance|
   if html_tag =~ /^<label/
-    sanitize %(<div class="field_with_errors">#{html_tag}</div>)
+    %(<div class="field_with_errors">#{html_tag}</div>).html_safe
   else
-    sanitize %(<div class="field_with_errors">#{html_tag}<label for="#{instance.send(:tag_id)}" class="message">#{instance.inspect} #{instance.error_message.first}</label></div>)
+    %(<div class="field_with_errors">#{html_tag}<label for="#{instance.send(:tag_id)}" class="message">#{instance.inspect} #{instance.error_message.first}</label></div>).html_safe
   end
 end
