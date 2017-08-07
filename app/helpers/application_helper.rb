04bc9c7abbcf4580d55dcb81da9199287d211eb2
Andy Porter <partydrone@me.com>
2017-08-07 15:08:08 -0600

    Fix entities in page title bug
    HTML entities were rendered in the `title` tag in the document head. This fixes that.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def full_title(page_title = nil)
     title = %w[Wavetronix]
     title << page_title if page_title.present?
-    title.join(' - ')
+    strip_tags title.join(' - ')
   end
 
   def humanize_locale(locale)
