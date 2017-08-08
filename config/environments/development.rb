833bcc9bef98c43a1d9d76fe7171fcc7e6f4f83d
Andy Porter <partydrone@me.com>
2017-08-08 11:30:21 -0600

    Precompile assets for production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,6 +42,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # Precompile assets locally
+  config.assets.prefix = '/dev-assets'
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
