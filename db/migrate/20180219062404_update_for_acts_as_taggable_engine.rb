ca54d80b4132b8d2fd9b73079e692a2d136a9c4f
Andy Porter <partydrone@me.com>
2018-02-18 23:57:42 -0700

    Add `acts-as-taggable-on` gem migration
    This migration was created by comparing the existing table structure and the
    changes created from the `acts-as-taggable-on` gem's migrations and
    constructing a composite. At the end of the day, this migration "fills in
    the blanks," as it were, to get things where they need to be.
    
    [X] add tagger polymorphic fields to taggings table
    [X] add context to taggings table
    [X] remove index [:taggable_id, :taggable_type] from taggings table
    
    [X] add unique index [:tag_id, :taggable_id, :taggable_type, :context, :tagger_id, :tagger_type] named 'taggings_idx' to taggings table
    
    [X] add index [:taggable_id, :taggable_type, :context] to taggings table
    
    [X] add individual indexes for :taggable_id, :taggable_type, :tagger_id, and :context
    [X] add index [:tagger_id, :tagger_type] to taggings table
    [X] add index [:taggable_id, :taggable_type, :tagger_id, :context] named 'taggings_idy' to taggings table

---
+++ db/migrate/20180219062404_update_for_acts_as_taggable_engine.rb
@@ -0,0 +1,21 @@
+class UpdateForActsAsTaggableEngine < ActiveRecord::Migration[5.0]
+  def change
+    change_table :taggings do |t|
+      t.references :tagger, polymorphic: true
+      t.string :context, limit: 128
+
+      t.remove_index [:taggable_id, :taggable_type]
+
+      t.index :taggable_id
+      t.index :taggable_type
+      t.index :tagger_id
+      t.index :context
+
+      t.index [:taggable_id, :taggable_type, :context]
+      t.index [:tagger_id, :tagger_type]
+
+      t.index [:tag_id, :taggable_id, :taggable_type, :context, :tagger_id, :tagger_type], name: 'taggings_idx', unique: true
+      t.index [:taggable_id, :taggable_type, :tagger_id, :context], name: 'taggings_idy'
+    end
+  end
+end
