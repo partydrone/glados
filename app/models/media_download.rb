052fd78f2021c2994f594c24c9d6c824beb5b074
Andy Porter <partydrone@me.com>
2018-03-07 09:50:29 -0700

    Fix media download bug
    Associating media downloads with a country included an empty value in the Array. I set a before save filter to remove that blank value from the array before persisting to the database.

--- app/models/media_download.rb
+++ app/models/media_download.rb
@@ -4,6 +4,10 @@ class MediaDownload < ApplicationRecord
   validates :description, presence: true
   validates :file, presence: true, on: :create
 
+  before_save do
+    self.country_ids.reject! { |item| item.blank? }
+  end
+
   def self.available(locale = I18n.locale)
     locale =~ /^([a-z]{2,2})(?:[-|_]([A-Z]{2,2}))?$/i
     query = where("'#{$2}' = ANY (country_ids)")
