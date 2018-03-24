fadc0142c4ba8a7228c07d317df4c4876fc454db
Andy Porter <partydrone@me.com>
2018-03-24 11:42:42 -0600

    Update refile configuration

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,27 +1,17 @@
 require 'refile/s3'
 
-case
-when Rails.env.production?
-  aws = {
-    credentials: Aws::Credentials.new(Rails.application.secrets.aws_access_key_id, Rails.application.secrets.aws_secret_access_key),
-    region:      'us-east-1',
-    bucket:      'com-wavetronix-www-uploads'
-  }
-  Refile.cdn_host = '//d1upf2v1quehpe.cloudfront.net'
-  # Refile.cdn_host = '//cdn.www.wavetronix.com'
-when Rails.env.staging?
-  aws = {
-    credentials: Aws::Credentials.new(Rails.application.secrets.aws_access_key_id, Rails.application.secrets.aws_secret_access_key),
-    region:      'us-east-1',
-    bucket:      'com-wavetronix-staging-glados-uploads'
-  }
-  Refile.cdn_host = '//uploads.glados.staging.wavetronix.com'
-end
-
 if Rails.env.production? || Rails.env.staging?
+  aws = {
+    credentials: Aws::Credentials.new(Rails.application.secrets.aws_access_key_id, Rails.application.secrets.aws_secret_access_key),
+    region:      Rails.application.secrets.aws_s3_region,
+    bucket:      Rails.application.secrets.aws_s3_bucket
+  }
+
+  Refile.cdn_host = "//#{Rails.application.secrets.aws_cloudfront_domain}"
   Refile.cache = Refile::S3.new(prefix: 'cache', **aws)
   Refile.store = Refile::S3.new(prefix: 'store', **aws)
 end
+
 
 Refile.processor :quality do |file|
   MiniMagick::Tool::Convert.new do |convert|
