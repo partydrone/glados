2e802b2522b7153422023326f9148ccccca90365
Andy Porter <partydrone@me.com>
2018-03-25 22:08:26 -0600

    Finish frequently asked questions in the admin section

--- app/controllers/admin/faq_topics_controller.rb
+++ app/controllers/admin/faq_topics_controller.rb
@@ -1,10 +1,53 @@
-class Admin::FaqTopicsController < ApplicationController
-  def index
-  end
+module Admin
+  class FaqTopicsController < BaseController
+    before_action :set_faq_topic, only: [:edit, :update, :destroy]
 
-  def new
-  end
+    def index
+      @faq_topics = FaqTopic.order(:title)
+      authorize @faq_topics
+    end
 
-  def edit
+    def new
+      @faq_topic = FaqTopic.new
+      authorize @faq_topic
+    end
+
+    def edit
+    end
+
+    def create
+      @faq_topic = FaqTopic.new(faq_topic_params)
+      authorize @faq_topic
+
+      if @faq_topic.save
+        redirect_to admin_faq_topics_path, notice: %(Saved "#{@faq_topic.title}" successfully.)
+      else
+        render :new
+      end
+    end
+
+    def update
+      if @faq_topic.update(faq_topic_params)
+        redirect_to admin_faq_topics_path, notice: %(Updated "#{@faq_topic.title} successfully.")
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @faq_topic.destroy
+      redirect_to admin_faq_topics_path, notice: %(Deleted "#{@faq_topic.title} successfully.")
+    end
+
+    private
+
+    def set_faq_topic
+      @faq_topic = FaqTopic.find(params[:id])
+      authorize @faq_topic
+    end
+
+    def faq_topic_params
+      params.require(:faq_topic).permit(:title)
+    end
   end
 end
