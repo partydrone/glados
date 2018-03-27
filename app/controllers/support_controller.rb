07fed47674a694fd7074e5e20c2714d88605532e
Andy Porter <partydrone@me.com>
2018-03-26 23:38:38 -0600

    Add FAQ to Support page
    Accordion-ized!

--- app/controllers/support_controller.rb
+++ app/controllers/support_controller.rb
@@ -1,7 +1,7 @@
 class SupportController < ApplicationController
   def index
     @product_types = ProductType.available
-    @products = Product.joins(:frequently_asked_questions, product_category: :product_type).select(:id, :name).reorder('product_types.position, products.name')
+    @faq_products = Product.joins(:frequently_asked_questions, product_category: :product_type).select(:id, :name).reorder('product_types.position, products.name')
     @return_material_authorization_policy_document = ReturnMaterialAuthorizationPolicyDocument.current
   end
 
